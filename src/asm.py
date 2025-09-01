80323c04587bc5c018752f03d85bc3b6ca4a5319
Eric P <cire.ttalp@gmail.com>
2025-08-31 19:32:06 -0700

    Pass chapter 3 --stage codegen

--- src/asm.py
+++ src/asm.py
@@ -17,7 +17,9 @@ class Imm():
 
 class Register_Enum(Enum):
     AX = auto()
+    DX = auto()
     R10 = auto()
+    R11 = auto()
 
 
 @dataclass
@@ -54,13 +56,36 @@ class Unary_Operator(Enum):
     NEGATION = auto()
 
 
+class Bin_Op(Enum):
+    ADD = auto()
+    SUB = auto()
+    MULT = auto()
+
+
 @dataclass
 class Unary:
     unary_operator: Unary_Operator
     operand: Operand
 
 
-Instruction = Mov | Allocate_Stack | Unary | Ret
+@dataclass
+class Binary:
+    binary_operator: Bin_Op
+    left: Operand
+    right: Operand
+
+
+@dataclass
+class Idiv:
+    operand: Operand
+
+
+@dataclass
+class Cdq:
+    pass
+
+
+Instruction = Mov | Allocate_Stack | Unary | Ret | Binary | Cdq | Idiv
 
 
 @dataclass
@@ -94,7 +119,19 @@ def convert_tacky_uop(node: tacky.Unary_Operator) -> Unary_Operator:
             raise RuntimeError(f'Unhandled unary operator {node}')
 
 
-def convert_tacky_instr(node: tacky.Instruction) -> Iterable[Instruction]:
+def convert_tacky_bop(node: tacky.Bin_Op) -> Bin_Op:
+    match node:
+        case tacky.Bin_Op.ADD:
+            return Bin_Op.ADD
+        case tacky.Bin_Op.SUBTRACT:
+            return Bin_Op.SUB
+        case tacky.Bin_Op.MULTIPLY:
+            return Bin_Op.MULT
+        case _:
+            raise RuntimeError(f'Unhandled unary operator {node}')
+
+
+def convert_tacky_instr(node: tacky.Instruction) -> tuple[Instruction, ...]:
     match node:
         case tacky.Return(val):
             src = convert_tacky_val(val)
@@ -104,6 +141,29 @@ def convert_tacky_instr(node: tacky.Instruction) -> Iterable[Instruction]:
             asm_dst = convert_tacky_val(dst)
             asm_op = convert_tacky_uop(unary_operator)
             return (Mov(asm_src, asm_dst), Unary(asm_op, asm_dst))
+        case tacky.Binary(tacky.Bin_Op.DIVIDE, src1, src2, dst):
+            asm_src1 = convert_tacky_val(src1)
+            asm_src2 = convert_tacky_val(src2)
+            asm_dst = convert_tacky_val(dst)
+            return (Mov(asm_src1, Register(Register_Enum.AX)),
+                    Cdq(),
+                    Idiv(asm_src2),
+                    Mov(Register(Register_Enum.AX), asm_dst))
+        case tacky.Binary(tacky.Bin_Op.REMAINDER, src1, src2, dst):
+            asm_src1 = convert_tacky_val(src1)
+            asm_src2 = convert_tacky_val(src2)
+            asm_dst = convert_tacky_val(dst)
+            return (Mov(asm_src1, Register(Register_Enum.AX)),
+                    Cdq(),
+                    Idiv(asm_src2),
+                    Mov(Register(Register_Enum.DX), asm_dst))
+        case tacky.Binary(operator, src1, src2, dst):
+            asm_bop = convert_tacky_bop(operator)
+            asm_src1 = convert_tacky_val(src1)
+            asm_src2 = convert_tacky_val(src2)
+            asm_dst = convert_tacky_val(dst)
+            return (Mov(asm_src1, asm_dst),
+                    Binary(asm_bop, asm_src2, asm_dst))
         case _:
             raise RuntimeError(f'Unhandled Instruction {node}')
 
@@ -160,6 +220,13 @@ def replace_psuedo(func: Function) -> int:
             case Unary(operator, dst):
                 new_dst = replace_operand(dst)
                 modified_instr.append(Unary(operator, new_dst))
+            case Binary(operator, left, right):
+                new_left = replace_operand(left)
+                new_right = replace_operand(right)
+                modified_instr.append(Binary(operator, new_left, new_right))
+            case Idiv(operand):
+                new_operand = replace_operand(operand)
+                modified_instr.append(Idiv(new_operand))
             case _:
                 modified_instr.append(instr)
 
@@ -178,6 +245,25 @@ def instruction_fixup(func: Function, alloc_count: int) -> None:
                 scratch = Register(Register_Enum.R10)
                 modified_instr.append(Mov(Stack(a), scratch))
                 modified_instr.append(Mov(scratch, Stack(b)))
+            case Binary(Bin_Op.ADD, Stack(a), Stack(b)):
+                scratch = Register(Register_Enum.R10)
+                modified_instr.append(Mov(Stack(a), scratch))
+                modified_instr.append(Binary(Bin_Op.ADD, scratch, Stack(b)))
+            case Binary(Bin_Op.SUB, Stack(a), Stack(b)):
+                scratch = Register(Register_Enum.R10)
+                modified_instr.append(Mov(Stack(a), scratch))
+                modified_instr.append(Binary(Bin_Op.SUB, scratch, Stack(b)))
+            case Binary(Bin_Op.MULT, src, Stack(a)):
+                # imul can't use a memory address as a dst
+                scratch = Register(Register_Enum.R11)
+                modified_instr.append(Mov(Stack(a), scratch))
+                modified_instr.append(Binary(Bin_Op.MULT, src, scratch))
+                modified_instr.append(Mov(scratch, Stack(a)))
+            case Idiv(Imm(a)):
+                # idiv can't use an immediate as an operand
+                scratch = Register(Register_Enum.R10)
+                modified_instr.append(Mov(Imm(a), scratch))
+                modified_instr.append(Idiv(scratch))
             case _:
                 modified_instr.append(instr)
     func.instructions = modified_instr

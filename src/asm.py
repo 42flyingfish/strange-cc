8648a4412c8fb4f3c8d1d6d5841b2c8307b0d9a6
Eric P <cire.ttalp@gmail.com>
2025-11-10 12:35:29 -0800

    Add operand size support
    Currently just B in a few places and DW elsewhere.

--- src/asm.py
+++ src/asm.py
@@ -18,9 +18,15 @@ class Register_Enum(Enum):
     AX = auto()
     CX = auto()
     DX = auto()
-    CL = auto()
     R10 = auto()
     R11 = auto()
+
+
+class Size(Enum):
+    B = auto()
+    W = auto()
+    L = auto()
+    Q = auto()
 
 
 @dataclass
@@ -43,6 +49,7 @@ class Ret():
 
 @dataclass
 class Mov():
+    size: Size
     src: Operand
     dst: Operand
 
@@ -71,24 +78,28 @@ class Bin_Op(Enum):
 @dataclass
 class Unary:
     unary_operator: Unary_Operator
+    size: Size
     operand: Operand
 
 
 @dataclass
 class Binary:
     binary_operator: Bin_Op
+    size: Size
     left: Operand
     right: Operand
 
 
 @dataclass
 class Cmp:
+    size: Size
     left: Operand
     right: Operand
 
 
 @dataclass
 class Idiv:
+    size: Size
     operand: Operand
 
 
@@ -217,33 +228,34 @@ def convert_tacky_instr(node: tacky.Instruction) -> tuple[Instruction, ...]:
     match node:
         case tacky.Return(val):
             src = convert_tacky_val(val)
-            return (Mov(src, Register(Register_Enum.AX)), Ret())
+            return (Mov(Size.L, src, Register(Register_Enum.AX)), Ret())
         case tacky.Unary(operator, src, dst):
             asm_src = convert_tacky_val(src)
             asm_dst = convert_tacky_val(dst)
             match operator:
                 case tacky.Unary_Operator.NOT:
-                    return (Cmp(Imm(0), asm_src),
-                            Mov(Imm(0), asm_dst),
+                    return (Cmp(Size.L, Imm(0), asm_src),
+                            Mov(Size.L, Imm(0), asm_dst),
                             SetCC(Cond_Code.E, asm_dst))
                 case _:
                     asm_op = convert_tacky_uop(operator)
-                    return (Mov(asm_src, asm_dst), Unary(asm_op, asm_dst))
+                    return (Mov(Size.L, asm_src, asm_dst),
+                            Unary(asm_op, Size.L, asm_dst))
         case tacky.Binary(operator, src1, src2, dst):
             asm_src1 = convert_tacky_val(src1)
             asm_src2 = convert_tacky_val(src2)
             asm_dst = convert_tacky_val(dst)
             match operator:
                 case tacky.Bin_Op.DIVIDE:
-                    return (Mov(asm_src1, Register(Register_Enum.AX)),
+                    return (Mov(Size.L, asm_src1, Register(Register_Enum.AX)),
                             Cdq(),
-                            Idiv(asm_src2),
-                            Mov(Register(Register_Enum.AX), asm_dst))
+                            Idiv(Size.L, asm_src2),
+                            Mov(Size.L, Register(Register_Enum.AX), asm_dst))
                 case tacky.Bin_Op.REMAINDER:
-                    return (Mov(asm_src1, Register(Register_Enum.AX)),
+                    return (Mov(Size.L, asm_src1, Register(Register_Enum.AX)),
                             Cdq(),
-                            Idiv(asm_src2),
-                            Mov(Register(Register_Enum.DX), asm_dst))
+                            Idiv(Size.L, asm_src2),
+                            Mov(Size.L, Register(Register_Enum.DX), asm_dst))
                 case (tacky.Bin_Op.GREATER_THAN
                       | tacky.Bin_Op.GREATER_EQUAL
                       | tacky.Bin_Op.LESS_THAN
@@ -251,27 +263,27 @@ def convert_tacky_instr(node: tacky.Instruction) -> tuple[Instruction, ...]:
                       | tacky.Bin_Op.EQUAL
                       | tacky.Bin_Op.NOT_EQUAL):
                     asm_cc = convert_tacky_relational(operator)
-                    return (Cmp(asm_src2, asm_src1),
-                            Mov(Imm(0), asm_dst),
+                    return (Cmp(Size.L, asm_src2, asm_src1),
+                            Mov(Size.L, Imm(0), asm_dst),
                             SetCC(asm_cc, asm_dst))
                 case _:
                     asm_bop = convert_tacky_bop(operator)
-                    return (Mov(asm_src1, asm_dst),
-                            Binary(asm_bop, asm_src2, asm_dst))
+                    return (Mov(Size.L, asm_src1, asm_dst),
+                            Binary(asm_bop, Size.L, asm_src2, asm_dst))
         case tacky.Jump(target):
             return (Jmp(target),)
         case tacky.JumpIfZero(condtion, target):
             asm_condition = convert_tacky_val(condtion)
-            return ((Cmp(Imm(0), asm_condition)),
+            return ((Cmp(Size.L, Imm(0), asm_condition)),
                     JmpCC(Cond_Code.E, target))
         case tacky.JumpIfNotZero(condition, target):
             asm_condition = convert_tacky_val(condition)
-            return ((Cmp(Imm(0), asm_condition)),
+            return ((Cmp(Size.L, Imm(0), asm_condition)),
                     JmpCC(Cond_Code.NE, target))
         case tacky.Copy(src, dst):
             asm_src = convert_tacky_val(src)
             asm_dst = convert_tacky_val(dst)
-            return (Mov(asm_src, asm_dst),)
+            return (Mov(Size.L, asm_src, asm_dst),)
         case tacky.Label(identifier):
             return (Label(identifier),)
         case _:
@@ -327,24 +339,25 @@ def replace_psuedo(func: Function) -> int:
     for instr in func.instructions:
         print(instr)
         match instr:
-            case Mov(src, dst):
+            case Mov(size, src, dst):
                 new_src = replace_operand(src)
                 new_dst = replace_operand(dst)
-                modified_instr.append(Mov(new_src, new_dst))
-            case Unary(operator, dst):
+                modified_instr.append(Mov(size, new_src, new_dst))
+            case Unary(operator, size, dst):
                 new_dst = replace_operand(dst)
-                modified_instr.append(Unary(operator, new_dst))
-            case Binary(operator, left, right):
+                modified_instr.append(Unary(operator, size, new_dst))
+            case Binary(operator, size, left, right):
                 new_left = replace_operand(left)
                 new_right = replace_operand(right)
-                modified_instr.append(Binary(operator, new_left, new_right))
-            case Idiv(operand):
+                modified_instr.append(
+                    Binary(operator, size, new_left, new_right))
+            case Idiv(size, operand):
                 new_operand = replace_operand(operand)
-                modified_instr.append(Idiv(new_operand))
-            case Cmp(left, right):
+                modified_instr.append(Idiv(size, new_operand))
+            case Cmp(size, left, right):
                 new_left = replace_operand(left)
                 new_right = replace_operand(right)
-                modified_instr.append(Cmp(new_left, new_right))
+                modified_instr.append(Cmp(size, new_left, new_right))
             case _:
                 modified_instr.append(instr)
 
@@ -358,62 +371,67 @@ def instruction_fixup(func: Function, alloc_count: int) -> None:
 
     for instr in func.instructions:
         match instr:
-            case Mov(Stack(a), Stack(b)):
+            case Mov(size, Stack(a), Stack(b)):
                 # Currently using %R10 as a scratch register
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Mov(scratch, Stack(b)))
-            case Binary(Bin_Op.ADD, Stack(a), Stack(b)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(Mov(size, scratch, Stack(b)))
+            case Binary(Bin_Op.ADD, size, Stack(a), Stack(b)):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.ADD, scratch, Stack(b)))
-            case Binary(Bin_Op.SUB, Stack(a), Stack(b)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(
+                    Binary(Bin_Op.ADD, size, scratch, Stack(b)))
+            case Binary(Bin_Op.SUB, size, Stack(a), Stack(b)):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.SUB, scratch, Stack(b)))
-            case Binary(Bin_Op.MULT, src, Stack(a)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(
+                    Binary(Bin_Op.SUB, size, scratch, Stack(b)))
+            case Binary(Bin_Op.MULT, size, src, Stack(a)):
                 # imul can't use a memory address as a dst
                 scratch = Register(Register_Enum.R11)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.MULT, src, scratch))
-                modified_instr.append(Mov(scratch, Stack(a)))
-            case Binary(Bin_Op.AND, Stack(a), Stack(b)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(Binary(Bin_Op.MULT, size, src, scratch))
+                modified_instr.append(Mov(size, scratch, Stack(a)))
+            case Binary(Bin_Op.AND, size, Stack(a), Stack(b)):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.AND, scratch, Stack(b)))
-            case Binary(Bin_Op.OR, Stack(a), Stack(b)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(
+                    Binary(Bin_Op.AND, size, scratch, Stack(b)))
+            case Binary(Bin_Op.OR, size, Stack(a), Stack(b)):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.OR, scratch, Stack(b)))
-            case Binary(Bin_Op.XOR, Stack(a), Stack(b)):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(
+                    Binary(Bin_Op.OR, size, scratch, Stack(b)))
+            case Binary(Bin_Op.XOR, size, Stack(a), Stack(b)):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Stack(a), scratch))
-                modified_instr.append(Binary(Bin_Op.XOR, scratch, Stack(b)))
-            case Binary(Bin_Op.RIGHT_SHIFT, Stack() as op1, op2):
+                modified_instr.append(Mov(size, Stack(a), scratch))
+                modified_instr.append(
+                    Binary(Bin_Op.XOR, size, scratch, Stack(b)))
+            case Binary(Bin_Op.RIGHT_SHIFT, size, Stack() as op1, op2):
                 scratch = Register(Register_Enum.CX)
-                cl = Register(Register_Enum.CL)
+                cl = Register(Register_Enum.CX)
                 modified_instr.extend(
-                    (Mov(op1, scratch),
-                     Binary(Bin_Op.RIGHT_SHIFT, cl, op2)))
-            case Binary(Bin_Op.LEFT_SHIFT, Stack() as op1, op2):
+                    (Mov(size, op1, scratch),
+                     Binary(Bin_Op.RIGHT_SHIFT, size, cl, op2)))
+            case Binary(Bin_Op.LEFT_SHIFT, size, Stack() as op1, op2):
                 scratch = Register(Register_Enum.CX)
-                cl = Register(Register_Enum.CL)
+                cl = Register(Register_Enum.CX)
                 modified_instr.extend(
-                    (Mov(op1, scratch),
-                     Binary(Bin_Op.LEFT_SHIFT, cl, op2)))
-            case Cmp(Stack() as left, Stack() as right):
+                    (Mov(size, op1, scratch),
+                     Binary(Bin_Op.LEFT_SHIFT, size, cl, op2)))
+            case Cmp(size, Stack() as left, Stack() as right):
                 scratch = Register(Register_Enum.R10)
-                modified_instr.extend((Mov(left, scratch),
-                                       Cmp(scratch, right)))
-            case Cmp(left, Imm() as right):
+                modified_instr.extend((Mov(size, left, scratch),
+                                       Cmp(size, scratch, right)))
+            case Cmp(size, left, Imm() as right):
                 scratch = Register(Register_Enum.R11)
-                modified_instr.extend((Mov(right, scratch),
-                                       Cmp(left, scratch)))
-            case Idiv(Imm(a)):
+                modified_instr.extend((Mov(size, right, scratch),
+                                       Cmp(size, left, scratch)))
+            case Idiv(size, Imm(a)):
                 # idiv can't use an immediate as an operand
                 scratch = Register(Register_Enum.R10)
-                modified_instr.append(Mov(Imm(a), scratch))
-                modified_instr.append(Idiv(scratch))
+                modified_instr.append(Mov(size, Imm(a), scratch))
+                modified_instr.append(Idiv(size, scratch))
             case _:
                 modified_instr.append(instr)
     func.instructions = modified_instr
--- src/code_emit.py
+++ src/code_emit.py
@@ -3,7 +3,21 @@ from collections.abc import Generator
 import asm
 
 
-def decode_operand(x) -> str:
+def decode_suffix(x: asm.Size) -> str:
+    match x:
+        case asm.Size.B:
+            return 'b'
+        case asm.Size.W:
+            return 'w'
+        case asm.Size.L:
+            return 'l'
+        case asm.Size.Q:
+            return 'q'
+        case _:
+            raise RuntimeError(f'Unhandled size{x}')
+
+
+def decode_32_operand(x) -> str:
     match x:
         case asm.Imm(val):
             return f'${val}'
@@ -13,8 +27,6 @@ def decode_operand(x) -> str:
             return '%ecx'
         case asm.Register(asm.Register_Enum.DX):
             return '%edx'
-        case asm.Register(asm.Register_Enum.CL):
-            return '%cl'
         case asm.Register(asm.Register_Enum.R10):
             return '%r10d'
         case asm.Register(asm.Register_Enum.R11):
@@ -25,30 +37,78 @@ def decode_operand(x) -> str:
             raise RuntimeError(f'Unhandled op {x}')
 
 
+def decode_8_operand(x) -> str:
+    match x:
+        case asm.Imm(val):
+            return f'${val}'
+        case asm.Register(asm.Register_Enum.AX):
+            return '%al'
+        case asm.Register(asm.Register_Enum.CX):
+            return '%cl'
+        case asm.Register(asm.Register_Enum.DX):
+            return '%dl'
+        case asm.Register(asm.Register_Enum.R10):
+            return '%r10b'
+        case asm.Register(asm.Register_Enum.R11):
+            return '%r11b'
+        case asm.Stack(offset):
+            return f'-{offset}(%rbp)'
+        case _:
+            raise RuntimeError(f'Unhandled op {x}')
+
+
 def decode_operator(x) -> str:
     match x:
         case asm.Unary_Operator.NEGATION:
-            return 'negl'
+            return 'neg'
         case asm.Unary_Operator.COMPLEMENT:
-            return 'notl'
+            return 'not'
         case asm.Bin_Op.ADD:
-            return 'addl'
+            return 'add'
         case asm.Bin_Op.SUB:
-            return 'subl'
+            return 'sub'
         case asm.Bin_Op.MULT:
-            return 'imull'
+            return 'imul'
         case asm.Bin_Op.LEFT_SHIFT:
-            return 'sall'
+            return 'sal'
         case asm.Bin_Op.RIGHT_SHIFT:
-            return 'sarl'
+            return 'sar'
         case asm.Bin_Op.AND:
-            return 'andl'
+            return 'and'
         case asm.Bin_Op.OR:
-            return 'orl'
+            return 'or'
         case asm.Bin_Op.XOR:
-            return 'xorl'
+            return 'xor'
         case _:
             raise RuntimeError(f'Unhandled op {x}')
+
+
+def decode_operand(op: asm.Operand, size: asm.Size) -> str:
+    match size:
+        case asm.Size.B:
+            return decode_8_operand(op)
+        case asm.Size.L:
+            return decode_32_operand(op)
+        case _:
+            raise RuntimeError(f'Unhandled size {size}')
+
+
+def decode_cond_code(x: asm.Cond_Code) -> str:
+    match x:
+        case asm.Cond_Code.E:
+            return 'e'
+        case asm.Cond_Code.NE:
+            return 'ne'
+        case asm.Cond_Code.L:
+            return 'l'
+        case asm.Cond_Code.LE:
+            return 'le'
+        case asm.Cond_Code.G:
+            return 'g'
+        case asm.Cond_Code.GE:
+            return 'ge'
+        case _:
+            raise RuntimeError(f'Unhandled cond_code {x}')
 
 
 def process_node(x) -> Generator[str]:
